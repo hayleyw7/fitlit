d253f69e5e43f27a3d2c913fc91ab69eb08431e7
Fara Akhatova <faraakhatova@gmail.com>
2021-07-11 12:02:22 -0600

    Add sample data for user test

--- src/User.js
+++ src/User.js
@@ -1,5 +1,7 @@
 class User {
-
+  constructor() {
+  
+  }
 }
 
 module.exports = User;
--- test/User-test.js
+++ test/User-test.js
@@ -1,2 +1,40 @@
 import { expect } from 'chai';
 import User from '../src/User';
+
+
+describe('User', () => {
+  let userData, hydration, sleep, activity, user, user1, user2;
+  beforeEach(() => {
+    userData = [
+      {
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+          16,
+          4,
+          8
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Jarvis Considine",
+        "address": "30086 Kathryn Port, Ciceroland NE 07273",
+        "email": "Dimitri.Bechtelar11@gmail.com",
+        "strideLength": 4.5,
+        "dailyStepGoal": 5000,
+        "friends": [
+          9,
+          18,
+          24,
+          19
+        ]
+      }
+    ]
+
+    hydration = [];
+  });
+});

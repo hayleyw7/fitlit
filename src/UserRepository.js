182c226f9e901b5e5ff225388dd5f22e140527fe
Fara Akhatova <faraakhatova@gmail.com>
2021-07-13 13:19:01 -0600

    Add test for water consumtion all times

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,3 @@
-// import User from '../src/User';
-
 
 class UserRepository {
   constructor(userData) {
@@ -8,6 +6,21 @@ class UserRepository {
   getUser(id) {
     const currentUser = this.users.find(user=> user.id === id)
     return currentUser;
+  }
+
+  waterConsumedAllTime(hydrationData) {
+    let hydrationAvg = hydrationData.reduce((total, userHydration) => {
+      return total + userHydration.numOunces
+    }, 0)
+    return Math.round(hydrationAvg / hydrationData.length)
+  }
+
+  waterConsumedOverWeek(hydrationData) {
+    let hydration = hydrationData.map(hydrate => {
+      hydrate.userID === this.users.id;
+      return hydrate.numOunces;
+    })
+    return hydration;
   }
 }
 
@@ -18,13 +31,13 @@ module.exports =  UserRepository;
 
 /// Methods to use in UserRepository Class/////
 // [ ]  `new UserRepository(data);`
-// - [ ]  A `UserRepository` holds onto all of the `User` objects
-// - [ ]  It should have a parameter to take in user `data`
-// - [ ]  It should have methods to determine:
-// - [ ]  Given a user’s ID, what is their user data?
+// - [X]  A `UserRepository` holds onto all of the `User` objects
+// - [X]  It should have a parameter to take in user `data`
+// - [X]  It should have methods to determine:
+// - [X]  Given a user’s ID, what is their user data?
 // - [ ]  The average step goal amongst all users
-//- [ ]  For a user (identified by their `userID` - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
- //[ ]  For a user, how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
+//- [X]  For a user (identified by their `userID` - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
+ //[X]  For a user, how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
  //- [ ]  For a user, their average sleep quality per day over all time
  //- [ ]  For a user, their sleep data over the course of the latest week (hours slept and quality of sleep)
 //- [ ]  For a user, their all-time average sleep quality and all-time average number of hours slept
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -37,4 +37,12 @@ describe('User Repository', () => {
   it('Should user/s data by it/s own unique id', () => {
     expect(userRepo.getUser(1)).to.equal(user1);
   });
+
+  it('Should return how much average water consumed for all times', () => {
+    expect(userRepo.waterConsumedAllTime(hydrationData)).to.equal(65);
+  })
+  it('Should find how many fluid ounces of water consumed each day over the course of a week', () => {
+    expect(userRepo.waterConsumedOverWeek(hydrationData).length).to.equal(7);
+  });
+
 });

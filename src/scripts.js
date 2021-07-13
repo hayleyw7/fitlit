e47c8ca392d68f39d712f228c8518437d3b4dae0
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-13 08:17:14 -0600

    Adjust bar chart

--- src/scripts.js
+++ src/scripts.js
@@ -32,17 +32,17 @@ const labels = [
 ];
 
 const data = {
-  labels: labels,
+  labels: ['You', 'Community'],
   datasets: [{
     label: 'My First dataset',
     backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
+    // borderColor: 'rgb(255, 99, 132)',
+    data: [40, 400],
   }]
 };
 
 const config = {
-  type: 'line',
+  type: 'horizontalBar',
   data,
   options: {}
 };

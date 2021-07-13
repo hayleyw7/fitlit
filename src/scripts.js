0e3d48e14bbbbd99d50c21673fc67baf6056f6de
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-13 08:04:02 -0600

    Add draft/skeleton of first chart

--- src/scripts.js
+++ src/scripts.js
@@ -13,3 +13,41 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 import userData from './data/users';
 import UserRepository from './UserRepository';
+
+
+
+/////////////////////////////////
+////////// chart tests //////////
+/////////////////////////////////
+//// this code will be moved ////
+/////////////////////////////////
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+
+const config = {
+  type: 'line',
+  data,
+  options: {}
+};
+
+  var myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );

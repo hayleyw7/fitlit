dcf37f300f4e90dec219a2a1f36dcf9321e8d4c7
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-13 17:48:55 -0600

    Add getAvgStepGoalOfAllUsers()

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -9,6 +9,13 @@ class UserRepository {
     return currentUser;
   }
 
+  getAvgStepGoalOfAllUsers() {
+    const totalStepGoalOfAllUsers = this.users.reduce((steps, user) => {
+      return steps += user.dailyStepGoal;
+    }, 0);
+    return totalStepGoalOfAllUsers / this.users.length;
+  }
+
   waterConsumedAllTime(hydrationData) {
     let hydrationAvg = hydrationData.reduce((total, userHydration) => {
       return total + userHydration.numOunces
@@ -23,6 +30,7 @@ class UserRepository {
     })
     return hydration;
   }
+
 }
 
 module.exports =  UserRepository;
@@ -32,12 +40,12 @@ module.exports =  UserRepository;
 
 /// Methods to use in UserRepository Class/////
 // [ ]  `new UserRepository(data);`
-// - [X]  A `UserRepository` holds onto all of the `User` objects
-// - [X]  It should have a parameter to take in user `data`
+// - [X]  A `UserRepository` holds onto all of the `User` objects
+// - [X]  It should have a parameter to take in user `data`
 // - [X]  It should have methods to determine:
 // - [X]  Given a user’s ID, what is their user data?
-// - [ ]  The average step goal amongst all users
-//- [X]  For a user (identified by their `userID` - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
+// - [x]  The average step goal amongst all users
+//- [X]  For a user (identified by their `userID` - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
  //[X]  For a user, how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
  //- [ ]  For a user, their average sleep quality per day over all time
  //- [ ]  For a user, their sleep data over the course of the latest week (hours slept and quality of sleep)
@@ -45,6 +53,6 @@ module.exports =  UserRepository;
 //- [ ]  For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 // - [ ]  For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 // - [ ]  For all users, the average sleep quality
-// - [ ]  Find all users who average a sleep quality greater than `3` for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+// - [ ]  Find all users who average a sleep quality greater than `3` for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 // - [ ]  For a user, how many minutes active did they average for a given week (7 days)?
 //- [ ]  For a user, find their all-time stair climbing record

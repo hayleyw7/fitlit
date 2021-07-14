83c918a47506af6c37398aec4736dd8657a8d4c7
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-14 14:56:20 -0600

    Add fetchActivityData

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,3 +4,11 @@ const fetchUserData = () => {
     .then(data => data)
     .catch(error => console.error("user promise rejected"));
 }
+
+const fetchActivityData = () => {
+  return fetch('http://localhost:3001/api/v1/activity')
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.error("activity promise rejected"));
+}
+

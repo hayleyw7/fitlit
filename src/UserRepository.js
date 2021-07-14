4ead8897bfd2082d86e78e08d2911d315f307827
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-13 19:54:01 -0600

    Add getMinActiveForWeek

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -3,6 +3,11 @@ class UserRepository {
   constructor(userData) {
     this.users = userData;
   }
+
+  getWeek() {
+    return `day.date === day1 || day.date === day2 || day.date === day3 || day.date === day4 || day.date === day5 || day.date === day6 || day.date === day7`;
+  }
+
   getUser(id) {
     const currentUser = this.users.find(user=> user.id === id)
     console.log(currentUser)
@@ -38,13 +43,13 @@ class UserRepository {
     return totalSleepQualityOfUser / sleepData.length;
   }
 
-  // The next 2 functions will need refactored A LOT but it works if weeks are entered really strangely (AKA one day at a time). This is a start, & it's definitely fixable.
+  // The next 3 functions will need refactored A LOT but it works if weeks are entered really strangely (AKA one day at a time). This is a start, & it's definitely fixable.
 
   getHoursSleptOverWeek(sleepData, id, day1, day2, day3, day4, day5, day6, day7) {
 		let result = [];
 		sleepData.forEach(day => {
-			if (day.date === day1 || day.date === day2 || day.date === day3 || day.date === day4 || day.date === day5 || day.date === day6 || day.date === day7) {
-				if (day.userID = id) {
+			if (this.getWeek()) {
+				if (day.userID ==== id) {
 					result.push(day.hoursSlept);
 				}
 			}
@@ -55,13 +60,31 @@ class UserRepository {
   getSleepQualityForWeek(sleepData, id, day1, day2, day3, day4, day5, day6, day7) {
 		let result = [];
 		sleepData.forEach(day => {
-			if (day.date === day1 || day.date === day2 || day.date === day3 || day.date === day4 || day.date === day5 || day.date === day6 || day.date === day7) {
-				if (day.userID = id) {
+			if (this.getWeek()) {
+				if (day.userID === id) {
 					result.push(day.sleepQuality);
 				}
 			}
 		})
 		return result;
+	}
+
+
+
+  function getWeek() {
+    return `day.date === day1 || day.date === day2 || day.date === day3 || day.date === day4 || day.date === day5 || day.date === day6 || day.date === day7`;
+  }
+
+  getMinActiveForWeek(activityData, id, day1, day2, day3, day4, day5, day6, day7) {
+		let dailyActivity = [];
+		activityData.forEach(day => {
+			if (this.getWeek()) {
+				if (day.userID === id) {
+					dailyActivity.push(day.minutesActive);
+				}
+			}
+		})
+		return (dailyActivity.reduce((dayA, dayB) => (dayA + dayB), 0)) / 7;
 	}
 
 }
@@ -87,5 +110,5 @@ module.exports =  UserRepository;
 // - [x]  For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 // - [x]  For all users, the average sleep quality
 // - [ ]  Find all users who average a sleep quality greater than `3` for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-// - [ ]  For a user, how many minutes active did they average for a given week (7 days)?
+// - [x]  For a user, how many minutes active did they average for a given week (7 days)?
 //- [ ]  For a user, find their all-time stair climbing record

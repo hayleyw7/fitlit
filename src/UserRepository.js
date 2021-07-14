3bff090e69a3b79181bcf404ca94be6008f26a2d
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-13 18:02:17 -0600

    Checkmark done test

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -54,7 +54,7 @@ module.exports =  UserRepository;
 // - [x]  The average step goal amongst all users
 //- [X]  For a user (identified by their `userID` - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
  //[X]  For a user, how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
- //- [ ]  For a user, their average sleep quality per day over all time
+ //- [x]  For a user, their average sleep quality per day over all time
  //- [ ]  For a user, their sleep data over the course of the latest week (hours slept and quality of sleep)
 //- [ ]  For a user, their all-time average sleep quality and all-time average number of hours slept
 //- [ ]  For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week

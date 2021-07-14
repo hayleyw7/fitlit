65a4f6d141db179a781d89f792ed29e8f6f57d30
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-13 19:24:19 -0600

    Add getSleepQualityForWeek

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -59,6 +59,32 @@ class UserRepository {
 		return result;
 	}
 
+  // The next 2 functions will need refactored A LOT but it works if weeks are entered really strangely (AKA one day at a time). This is a start, & it's definitely fixable.
+
+  getHoursSleptOverWeek(sleepData, id, day1, day2, day3, day4, day5, day6, day7) {
+		let result = [];
+
+		sleepData.forEach(day => {
+			if (day.date === day1 || day.date === day2 || day.date === day3 || day.date === day4 || day.date === day5 || day.date === day6 || day.date === day7) {
+				if (day.userID = id) {
+					result.push(day.hoursSlept);
+				}
+			}
+		})
+		return result;
+	}
+
+  getSleepQualityForWeek(id, day1, day2, day3, day4, day5, day6, day7) {
+		let result = [];
+		sleepData.forEach(day => {
+			if (day.date === day1 || day.date === day2 || day.date === day3 || day.date === day4 || day.date === day5 || day.date === day6 || day.date === day7) {
+				if (day.userID = id) {
+					result.push(day.sleepQuality);
+				}
+			}
+		})
+		return result;
+	}
 
 }
 
@@ -80,7 +106,7 @@ module.exports =  UserRepository;
  //- [ ]  For a user, their sleep data over the course of the latest week (hours slept and quality of sleep)
 //- [ ]  For a user, their all-time average sleep quality and all-time average number of hours slept
 //- [x]  For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-// - [ ]  For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
+// - [x]  For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 // - [x]  For all users, the average sleep quality
 // - [ ]  Find all users who average a sleep quality greater than `3` for a given week (7 days) - you should be able to calculate this for any week, not just the latest week
 // - [ ]  For a user, how many minutes active did they average for a given week (7 days)?
